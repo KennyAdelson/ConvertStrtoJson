5eb79fab9cec9a839859382240038c97ff709860
P41劉明昕 <001668@i-pass.com.tw>
2017-11-16 16:10:59 +0800

    version 1

---
+++ ConvertStrtoJson/Convertlib.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConvertStrtoJson
+{
+    public static class Convertlib
+    {
+        public static string StrtoJson(object cls)
+        {
+            try
+            {
+                string json_str = JsonConvert.SerializeObject(cls);
+
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+
+            return "";
+        }
+
+
+
+    }
+}
